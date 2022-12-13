1247a38add1ce66268e37fcb14136712e1d474a1
unknown <mosharafctg111@gmail.com>
2022-12-13 22:49:49 +0600

    added the mongobd

--- src/Pages/Home/Project/Project.js
+++ src/Pages/Home/Project/Project.js
@@ -11,51 +11,22 @@ import proj2 from '../../../Assets/projects/turist guide.png'
 import proj3 from '../../../Assets/projects/dream learn.png'
 import CustomTitle from '../../../components/CustomTitle';
 import ProjectsCard from './ProjectsCard';
+import { useQuery } from '@tanstack/react-query';
 
 const Project = () => {
-    const projects = [
-        {
-            id: 1,
-            title: 'Laptop Market',
-            image: proj1,
-            tools: [
-                {img: img4, title: 'React JS'},
-                {img: img5, title: 'MongoDb'},
-                {img: img6, title: 'Firebase'},
-                {img: img8, title: 'Tailwind'},
-                {img: img13, title: 'Vercel'},
-            ],
-            demolink: 'https://laptop-resale-market-6f17c.web.app/',
-            codelink: 'https://github.com/Mosharaf12/laptop-resale-client-12'
-        },
-        {
-            id: 2,
-            title: 'Tourist Guide',
-            image: proj2,
-            tools: [
-                {img: img4, title: 'React JS'},
-                {img: img5, title: 'MongoDb'},
-                {img: img6, title: 'Firebase'},
-                {img: img8, title: 'Tailwind'},
-                {img: img13, title: 'Vercel'},
-            ],
-            demolink: 'https://tourist-guide-client-8307d.web.app/',
-            codelink: 'https://github.com/Mosharaf12/tourist-guide-client-11'
-        },
-        {
-            id: 3,
-            title: 'Dream learn',
-            image: proj3,
-            tools: [
-                {img: img4, title: 'React JS'},
-                {img: img6, title: 'Firebase'},
-                {img: img8, title: 'Tailwind'},
-                {img: img13, title: 'Vercel'},
-            ],
-            demolink: 'https://online-education-client.web.app/',
-            codelink: 'https://github.com/Mosharaf12/dream-learn-client-10'
+
+    const {data: projects = [], refetch} = useQuery({
+        queryKey:['projects'],
+        queryFn: async()=>{
+            const res = await fetch(`http://localhost:5000/projects`)
+            const data = await res.json();
+            return data;
         }
-    ]
+        
+    })
+    refetch();
+
+
     return (
         <div className='px-5 py-20' id='projects'>
         <CustomTitle>PROJECTS</CustomTitle>
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {QueryClient, QueryClientProvider,}from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient()
 root.render(
   <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
+  </QueryClientProvider>
   </React.StrictMode>
 );
 
